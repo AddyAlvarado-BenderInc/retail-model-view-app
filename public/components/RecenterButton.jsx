be9e18335f6b8f57cce96c98743927a59b399be2
AddyAlvarado-BenderInc <163353963+AddyAlvarado-BenderInc@users.noreply.github.com>
2024-09-10 08:09:30 -0500

    Update RecenterButton.jsx

--- public/components/RecenterButton.jsx
+++ public/components/RecenterButton.jsx
@@ -1,42 +1,45 @@
 import React from 'react';
 import { Vector3 } from 'three';
-
+ 
 function RecenterButton({ orbitRef }) {
   const recenter = () => {
     if (orbitRef.current) {
       const { current } = orbitRef;
-
+ 
       const start = {
         cameraPosition: { ...current.object.position },
         targetPosition: { ...current.target },
       };
-
+ 
+      // Check screen size and adjust target camera position
+      const isSmallScreen = window.innerWidth < 700;
+      const targetCameraPosition = isSmallScreen ? new Vector3(40, 6.5, 0) : new Vector3(25, 6.5, 0);
       const target = {
-        cameraPosition: new Vector3(25, 6.5, 0),
+        cameraPosition: targetCameraPosition, // Adjust the camera distance based on screen size
         targetPosition: new Vector3(0, 0, 0),
       };
-
+ 
       let t = 0;
-
+ 
       const animateRecenter = () => {
         t += 0.02;
-
+ 
         current.object.position.lerpVectors(start.cameraPosition, target.cameraPosition, t);
         current.target.lerpVectors(start.targetPosition, target.targetPosition, t);
         current.update();
-
+ 
         if (t < 1) requestAnimationFrame(animateRecenter);
       };
-
+ 
       animateRecenter();
     }
   };
-
+ 
   return (
-    <button className="recenter-button" onClick={recenter}>
+<button className="recenter-button" onClick={recenter}>
       Recenter
-    </button>
+</button>
   );
 }
-
+ 
 export default RecenterButton;
